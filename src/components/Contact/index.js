f7b89d4171a990584e916fd1a4768253e58d1983
dsseo0826 <50734046+dsseo0826@users.noreply.github.com>
2021-03-17 16:18:32 -0400

    update contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,62 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ContactSection, ContactTitle, ContactContainer, ContactInfo, ContactSubtitleH3, ContactText, FormBox, ContactInputs, FormLabel, FormInput, FormButton, TextArea }
     from './ContactElements'
 function Contact() {
+
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        if (window.location.search.includes('success=true')) {
+            setSuccess(true);
+        }
+    }, []);
     return (
 
-        <form
-            name='contact'
-            method='post'
-            data-netlify='ture'
-            onSubmit='submit'>
-            <input type='hidden' name='form-name' value='contact' />
-            <div>
-                <input
-                    type='text'
-                    name='form-name'
-                    require placeholder='Name' />
-
-            </div>
-            <input
-                id='email'
-                name='email'
-                type='email'
-                require placeholder='Email' />
-            <label htmlFor='for'>Text</label>
-            <input type='text' />
-            <button type='submit'>Submit</button>
-        </form >
-        /* <ContactSection id='contact'>
-                    <ContactTitle>Contact</ContactTitle>
-                    <ContactContainer>
-                        <ContactInfo>
-                            <ContactSubtitleH3>Email</ContactSubtitleH3>
-                            <ContactText>dev.kateseo@gmail.com</ContactText>
-                        </ContactInfo>
-                        <FormBox>
-                            <form
-                                name='contact'
-                                method='post'
-                                data-netlify='ture'
-                                onSubmit='submit'>
-                                <ContactInputs type='hidden' name='form-name' value='contact'>
-                                    <FormInput
-                                        type='text'
-                                        name='form-name'
-                                        require placeholder='Name' />
-                                    <FormInput
-                                        id='email'
-                                        name='email'
-                                        type='email'
-                                        require placeholder='Email' />
-                                </ContactInputs>
-                                <FormLabel htmlFor='for'>Text</FormLabel>
-                                <TextArea type='text' />
-                                <FormButton type='submit'>Submit</FormButton>
-                            </form>
-                        </FormBox>
-                    </ContactContainer>
-                // </ContactSection> */
+        <>
+            <ContactSection id='contact'>
+                <ContactTitle>Contact</ContactTitle>
+                {success && (
+                    <p style={{ color: 'yellowgreen' }} >Thanks for your message!</p>
+                )}
+                <ContactContainer>
+                    <ContactInfo>
+                        <ContactSubtitleH3>Email</ContactSubtitleH3>
+                        <ContactText>dev.kateseo@gmail.com</ContactText>
+                    </ContactInfo>
+                    <FormBox>
+                        <form
+                            name='contact'
+                            method='post'
+                            action='/contact/?success=true'
+                            data-netlify='true'
+                            onSubmit='submit'>
+                            <ContactInputs type='hidden' name='form-name' value='contact'>
+                                <input type='hidden' name='form-name' value='contact' />
+                                <FormInput
+                                    type='text'
+                                    name='name'
+                                    require placeholder='Name' />
+                                <FormInput
+                                    id='email'
+                                    name='email'
+                                    type='email'
+                                    require placeholder='Email' />
+                            </ContactInputs>
+                            <FormLabel htmlFor='for'>Text</FormLabel>
+                            <TextArea type='text' name='message' />
+                            <FormButton type='submit'>Submit</FormButton>
+                        </form>
+                    </FormBox>
+                </ContactContainer>
+            </ContactSection>
+        </ >
 
     )
 }
